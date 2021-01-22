e4c8c1ff627f1fb1df038c983b4b8ca926e61a26
Akshay Khemaney <khemaney.a@etoncollege.org.uk>
2021-01-22 11:59:06 +0000

    Initial Commit

---
+++ TamagotchiApp/TamagotchiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TamagotchiAppApp.swift
+//  TamagotchiApp
+//
+//  Created by Khemaney, Akshay (SPH) on 22/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct TamagotchiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
